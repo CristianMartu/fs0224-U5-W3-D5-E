3e7c37e0254667f68e2f47f10736d8cb562de095
CristianMartu <martuccicristian18@gmail.com>
2024-07-05 10:14:19 +0200

    Initial commit

---
+++ src/main/java/cristianmartucci/U5_W3_D5_E/U5W3D5EApplication.java
@@ -0,0 +1,13 @@
+package cristianmartucci.U5_W3_D5_E;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5EApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5EApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cristianmartucci/U5_W3_D5_E/U5W3D5EApplicationTests.java
@@ -0,0 +1,13 @@
+package cristianmartucci.U5_W3_D5_E;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5EApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
